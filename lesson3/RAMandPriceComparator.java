da4b8d8c5b7017cfce5f2d2163ad4a7453df00ec
Kutueva Olga <kutueva.olga@gmail.com>
2023-02-22 20:17:40 +0300

    ДЗ 3. Убраны лишние комментарии

--- lesson3/RAMandPriceComparator.java
+++ lesson3/RAMandPriceComparator.java
@@ -8,21 +8,6 @@ public class RAMandPriceComparator implements Comparator<Notebook>{
         if (o1.ram == o2.ram)
             return Double.compare(o1.price, o2.price);   
         else
-            return Integer.compare(o1.ram, o2.ram);  
-        
-        
-        // if (o1.ram > o2.ram)
-        //     return 1;
-        // else if (o1.ram < o2.ram)
-        //     return -1;
-        // else if (o1.price > o2.price)
-        //     return 1;
-        // else if (o1.price < o2.price)
-        //     return -1;
-        // else
-        //     return 0;
-
-
-        //return Integer.compare(o1.ram, o2.ram);
+            return Integer.compare(o1.ram, o2.ram);
     }
 }
