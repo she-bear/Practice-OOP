888514c63ff5cd2851c0f447ca998043abe69bda
Kutueva Olga <kutueva.olga@gmail.com>
2023-03-01 13:52:27 +0300

    ДЗ 4. Реализация класса Box и комментарии в Main

--- lesson4/Box.java
+++ lesson4/Box.java
@@ -1,22 +1,56 @@
 package lesson4;
 
-public class Box /*<T>*/ /* FIXME Исправлять тут! */ {
+import java.util.ArrayList;
+import java.util.List;
 
-    // Нужно реализовать хранение фрукто в коробке Box
+// Object <- Fruit <- Apple
+// Object <- Fruit <- Orange
+// Object <- Fruit <- Apple <- Golden Apple
 
-    public void add(Object fruit) {
-        // FIXME: 21.02.2023 Написать логику сохранения
-        throw new UnsupportedOperationException();
+//             Super <- -> Extends
+// Object <- Fruit <- Apple <- Golden Apple
+public class Box <T extends Fruit> { // сюда м.б. помещен любой наследник класса Fruit
+
+    List<T> fruits = new ArrayList<T>();
+
+    public void add(T fruit) {
+        fruits.add(fruit);
     }
 
     public double getWeight() {
-        // FIXME: 21.02.2023
-        throw new UnsupportedOperationException();
+        double weight = 0;
+        
+        for (T fruit : fruits) {
+            weight += fruit.getWeight();
+        }
+        
+        return weight;
     }
 
-    public void moveTo(Box anotherBox) {
-        // FIXME: 21.02.2023
-        throw new UnsupportedOperationException();
+    // если T = Apple, то запись <? super T> означает, что пройдут Object <- Fruit <- Apple
+    public void moveTo(Box<? super T> anotherBox) { // параметризуем принимаемый аргумент
+        for (T fruit : fruits) {
+            // T = Apple : fruit
+            // anotherbox     fruit
+            // Apple < Apple +
+            // Fruit < Apple +
+            // Object < Apple +
+            anotherBox.add(fruit);
+        }
+        fruits = new ArrayList<T>();
     }
 
+    // расмотрим случай, если мы захотим GoldenApple переложить в Box<Apple>
+    // если T = Apple, то запись <? extends T> означает, что пройдут Apple <- GoldenApple
+    // public void moveTo_2(Box<? extends T> anotherBox) {
+    //     for (T fruit : fruits) {
+    //         // T = Apple : fruit
+    //         // Apple < Apple
+    //         // GoldenApple < Apple не работает для метода add!!!
+    //         anotherBox.add(fruit);
+    //     }
+    //     fruits = new ArrayList<T>();
+    // }
+
+    // ВЫВОД: такой метод реализовать в данном контексе нельзя
 }
